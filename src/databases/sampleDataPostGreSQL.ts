03b801806c4f4632260a9833e0a87f5739a1a925
Nick Coughlin <coughlin.nicholas@gmail.com>
2024-09-29 19:55:09 -0700

    db username and password now use config values

--- src/databases/sampleDataPostGreSQL.ts
+++ src/databases/sampleDataPostGreSQL.ts
@@ -1,4 +1,11 @@
+import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
+
+const config = new pulumi.Config();
+
+// these values should be moved to a secret manager
+const db_username = config.require("db_username");
+const db_password = config.require("db_password");
 
 export const sampleDataDB = new aws.rds.Instance("sample-data-postgre-sql", {
   allocatedStorage: 10,
@@ -6,8 +13,8 @@ export const sampleDataDB = new aws.rds.Instance("sample-data-postgre-sql", {
   engine: "postgres",
   engineVersion: "16.3",
   instanceClass: aws.rds.InstanceType.T4G_Micro,
-  username: "foo",
-  password: "foobarbaz",
+  username: db_username,
+  password: db_password,
   parameterGroupName: "default.postgres16",
   skipFinalSnapshot: true,
   publiclyAccessible: true,
