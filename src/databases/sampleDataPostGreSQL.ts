4222ec6c4ca9d6cc5df0dfb40d7bc6fb102dea9c
Nick Coughlin <coughlin.nicholas@gmail.com>
2024-09-30 09:45:22 -0700

    db creds pass into lambda as event

--- src/databases/sampleDataPostGreSQL.ts
+++ src/databases/sampleDataPostGreSQL.ts
@@ -9,77 +9,86 @@ const db_password = config.require("db_password");
 const db_name = config.require("db_name");
 
 export const sampleDataDB = new aws.rds.Instance("sample-data-postgre-sql", {
-    allocatedStorage: 10,
-    dbName: db_name,
-    engine: "postgres",
-    engineVersion: "16.3",
-    instanceClass: aws.rds.InstanceType.T4G_Micro,
-    username: db_username,
-    password: db_password,
-    parameterGroupName: "default.postgres16",
-    skipFinalSnapshot: true,
-    publiclyAccessible: true,
-    port: 5432,
+  allocatedStorage: 10,
+  dbName: db_name,
+  engine: "postgres",
+  engineVersion: "16.3",
+  instanceClass: aws.rds.InstanceType.T4G_Micro,
+  username: db_username,
+  password: db_password,
+  parameterGroupName: "default.postgres16",
+  skipFinalSnapshot: true,
+  publiclyAccessible: true,
+  port: 5432,
 });
 
 type eventType = {
-    dbEndpoint: string;
+  db_endpoint: string;
+  db_name: string;
+  db_username: string;
+  db_password: string;
 };
 
 // Create a schema and a table using a Lambda function
-const createSchemaAndTable = new aws.lambda.CallbackFunction(
-    "createSchemaAndTable",
-    {
-        callback: async (event: eventType, context) => {
-          console.log("🌎 event", event);
+const createSampleDbSchemaAndTable = new aws.lambda.CallbackFunction(
+  "createSchemaAndTable",
+  {
+    callback: async (event: eventType, context) => {
+      console.log("🌎 event", event);
 
-            const { Client } = require("pg");
-            const client = new Client({
-                host: event.dbEndpoint,
-                database: db_name,
-                user: db_username,
-                password: db_password,
-            });
+      const { Client } = require("pg");
+      const client = new Client({
+        host: event.db_endpoint,
+        database: event.db_name,
+        user: event.db_username,
+        password: event.db_password,
+      });
 
-            await client.connect();
-            await client.query(`CREATE SCHEMA IF NOT EXISTS myschema`);
-            await client.query(
-                `CREATE TABLE IF NOT EXISTS myschema.mytable (id SERIAL PRIMARY KEY, data VARCHAR(100))`
-            );
-            await client.end();
-        },
-    }
+      await client.connect();
+      await client.query(`CREATE SCHEMA IF NOT EXISTS myschema`);
+      await client.query(
+        `CREATE TABLE IF NOT EXISTS myschema.mytable (id SERIAL PRIMARY KEY, data VARCHAR(100))`
+      );
+      await client.end();
+    },
+  }
 );
 
 // Create an SNS Topic
-const snsTopic = new aws.sns.Topic("rdsEventTopic");
+const snsTopic = new aws.sns.Topic("sampleDataDbCreationTopic");
 
 // Subscribe the Lambda function to the SNS Topic
-const snsSubscription = new aws.sns.TopicSubscription("snsSubscription", {
+const snsSubscription = new aws.sns.TopicSubscription(
+  "sampleDataDbCreationSnsSubscription",
+  {
     topic: snsTopic.arn,
     protocol: "lambda",
-    endpoint: createSchemaAndTable.arn,
-});
+    endpoint: createSampleDbSchemaAndTable.arn,
+  }
+);
 
 // Grant SNS permission to invoke the Lambda function
-const lambdaPermission = new aws.lambda.Permission("lambdaPermission", {
+const lambdaPermission = new aws.lambda.Permission(
+  "createSchemaAndTableLambdaPermission",
+  {
     action: "lambda:InvokeFunction",
-    function: createSchemaAndTable.name,
+    function: createSampleDbSchemaAndTable.name,
     principal: "sns.amazonaws.com",
     sourceArn: snsTopic.arn,
-});
+  }
+);
 
 // Create an RDS Event Subscription
 const rdsInvoke = new aws.rds.EventSubscription(
-    "rdsInvoke",
-    {
-        snsTopic: snsTopic.arn,
-        sourceType: "db-instance",
-        // sourceIds: [sampleDataDB.id],
-        sourceIds: [sampleDataDB.identifier],
-        eventCategories: ["creation"],
-    },
-    { dependsOn: [sampleDataDB, snsSubscription, lambdaPermission] }
+  "rdsInvoke",
+  {
+    snsTopic: snsTopic.arn,
+    sourceType: "db-instance",
+    // sourceIds: [sampleDataDB.id],
+    sourceIds: [sampleDataDB.identifier],
+    eventCategories: ["creation"],
+  },
+  { dependsOn: [sampleDataDB, snsSubscription, lambdaPermission] }
 );
 
 // Export the database endpoint
